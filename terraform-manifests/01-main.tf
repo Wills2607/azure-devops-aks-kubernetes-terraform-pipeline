020050808c1eba6424dafb77c0cf47e03b8ded6b
William Robert C <wills@Williams-Air.lan>
2023-03-27 13:42:37 -0500

    azure devops aks kubernetes terraform pipeline setup

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -28,10 +28,10 @@ terraform {
 
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
-    #resource_group_name   = "terraform-storage-rg"
-    #storage_account_name  = "terraformstatexlrwdrzs"
-    #container_name        = "tfstatefiles"
-    #key                   = "terraform-custom-vnet.tfstate"
+    resource_group_name   = "devops-resource-group"
+    storage_account_name  = "terraformazureaks_1679941407264"
+    container_name        = "tfstatefiles"
+    key                   = "terraform-custom-vnet.tfstate"
   }  
 }
 
